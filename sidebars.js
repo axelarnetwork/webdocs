4ddd98d6fbc2aab718d7caea8d6343824a09d089
Gus Gutoski <gus.gutoski@protonmail.com>
2022-03-01 17:02:45 -0500

    rename testnet-validator-setup -> manual

--- sidebars.js
+++ sidebars.js
@@ -56,7 +56,7 @@ const sidebars = {
                 'roles/validator/setup/register-broadcaster',
                 'roles/validator/setup/stake-axl',
                 'roles/validator/setup/health-check',
-                'roles/validator/setup/testnet-validator-setup',
+                'roles/validator/setup/manual',
               ],
             },
             {
